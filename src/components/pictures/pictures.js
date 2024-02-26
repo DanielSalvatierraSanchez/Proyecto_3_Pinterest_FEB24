23a350ca858fb81a03324ecc8f497c883dd5fc54
Daniel Salvatierra Sanchez <147101731+DanielSalvatierraSanchez@users.noreply.github.com>
2024-02-26 18:17:48 +0100

    Add files via upload

---
+++ src/components/pictures/pictures.js
@@ -0,0 +1,14 @@
+// crear function que pinta las imagenes obtenidas de la API
+export function renderPictures(data) {
+  for (const image of data) {
+    const main = document.querySelector('.image-container');
+    const imageRandom = document.createElement('img');
+    imageRandom.className = 'image-random';
+    imageRandom.src = image.urls.small;
+    const imageLink = document.createElement('a');
+    imageLink.href = image.links.html;
+    imageLink.target = '_blank';
+    imageLink.appendChild(imageRandom);
+    main.appendChild(imageLink);
+  };
+}
