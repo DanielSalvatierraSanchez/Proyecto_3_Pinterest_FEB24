4954feca23ca01691739eddb55e4308420568e0b
Daniel Salvatierra Sanchez <147101731+DanielSalvatierraSanchez@users.noreply.github.com>
2024-03-10 23:46:34 +0100

    Delete src/components/gallery directory

--- src/components/gallery/gallery.js
+++
@@ -1,9 +0,0 @@
-import './gallery.css';
-
-// crear contenedor para las imagenes
-export const createImageContainer = () => {
-  const body = document.querySelector('body');
-  const main = document.createElement('main');
-  main.className = 'image-container';
-  body.appendChild(main);
-};
